50bdb95303535a06a50f307d26bfc2b5f0155c0c
Nico Vandenbroucke <nico.vandenbroucke@wgkwvl.be>
2022-10-11 18:44:44 +0200

    week van 03 10

--- 2. Vanilla Javascript/intro/index.js
+++ 2. Vanilla Javascript/intro/index.js
@@ -18,3 +18,15 @@ reassign.push("id4");
 
 console.log(items);
 
+
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+
+console.log(sumWithInitial);
